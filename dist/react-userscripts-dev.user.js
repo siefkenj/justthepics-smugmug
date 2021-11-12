db989af325ab3ab754480974598b0493e2098ad2
Jason Siefken <siefkenj@gmail.com>
2021-11-12 14:02:43 -0500

    Port to React Userscript

---
+++ dist/react-userscripts-dev.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name     React Userscripts Smugmug
+// @version  1.1
+// @description Development mode for React Userscripts Smugmug.
+// @include https://*smugmug.com/*
+// @grant    none
+// ==/UserScript==
+
+"use strict";
+
+function log(...args) {
+    console.log("%cUserscript:", "color: purple; font-weight: bold", ...args);
+}
+
+log("Dev mode started");
+
+async function main() {
+    const resp = await fetch("http://localhost:8124/static/js/main.js");
+    const script = await resp.text();
+    log("Got Dev script");
+    eval(script);
+    log("Dev script evaled");
+}
+
+// Make sure we run once at the start
+main.bind({})().catch((e) => {
+    log("ERROR", e);
+});
---
+++ userscript/src/userscript-header.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name     React Userscripts Smugmug
+// @namespace https://smugmug.com/
+// @version  1.1
+// @description A sample userscript built using react
+// @include https://*smugmug.com/*
+// @grant    none
+// ==/UserScript==
+
